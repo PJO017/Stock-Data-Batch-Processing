c0647b8ec5469d6ac1ba98510bf44741adf39d67
Paul Ofremu Jr <pauljrofremu@gmail.com>
2023-04-15 16:38:01 -0400

    Add new tab to show visualizations sent to client

--- Source/Client/client.js
+++ Source/Client/client.js
@@ -1,7 +1,30 @@
-fetch("http://localhost:8000/visualize")
-  .then(function(response) {
-    console.log("success!", response);
-  })
-  .catch(function(err) {
-    console.warn("Error", err);
-  });
+function sendRequest() {
+  // Get the text from the text box
+  var ticker = document.getElementById("textbox").value;
+
+  // Define the HTTP request options
+  var options = {
+    method: "POST",
+    body: ticker,
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  };
+
+  // Send the HTTP request with the fetch() method
+  fetch("http://localhost:8000/visualize", options)
+    .then(function(response) {
+      if (response.status == 200) {
+        return response.text();
+      }
+      throw new Error("Network response was not ok.");
+    })
+    .then(function(html) {
+      var newWindow = open();
+      newWindow.document.write(html);
+      newWindow.document.close();
+    })
+    .catch(function(error) {
+      console.log("There was a problem with the fetch operation:", error);
+    });
+}
