7eae944901a6facb633d3f98d65ddbe709cdaa38
Paul Ofremu Jr <pauljrofremu@gmail.com>
2023-04-15 10:42:45 -0400

    Add visualization and client

--- Source/Server/server.py
+++ Source/Server/server.py
@@ -1,18 +1,19 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from visualizatons import visualize
+from visualizatons import generate_visualizations
 import json
 
 
 class MyRequestHandler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        data = self.rfile.read(content_length).decode('utf-8')
-        message = json.dumps(visualize(data)).encode('utf-8')
+    def do_GET(self):
+        if self.path == '/visualize':
+            content_length = int(self.headers['Content-Length'])
+            data = self.rfile.read(content_length).decode('utf-8')
+            message = json.dumps(generate_visualizations(data)).encode('utf-8')
 
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(message)
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(message)
 
 
 def run():
--- Source/Server/visualizatons.py
+++ Source/Server/visualizatons.py
@@ -1,16 +1,42 @@
+import numpy as np
 from cassandra.cluster import Cluster
+from bokeh.plotting import figure, output_file, show
 
 # replace localhost with your Cassandra host IP
 cluster = Cluster(['localhost'])
 session = cluster.connect()
 
 
-def visualize(ticker):
-
+def queryCluster(ticker):
     # Create prepared statement
     get_stock = session.prepare("""
         SELECT * FROM main.stock WHERE TICKER='{}';
         """.format(ticker))
 
-    ticker_data = session.execute(get_stock).one()
-    return ticker_data
+    return session.execute(get_stock).one()
+
+
+def generate_visualizations(ticker):
+    data = queryCluster(ticker)
+    dates = [date.date() for date in data.dates]
+
+    # Visualize ATR
+    atr = data.atr
+    output_file("atr.html")
+    graph = figure(x_axis_type="datetime", title="{} ATR".format(ticker))
+    graph.xaxis.axis_label = 'Date'
+    graph.yaxis.axis_label = 'ATR (14 day)'
+
+    x_axis_coordinates = np.array(dates, dtype=np.datetime64)
+    y_axis_coordinates = np.array(atr)
+    color = "lightblue"
+    legend_label = ticker
+    graph.line(x_axis_coordinates, y_axis_coordinates,
+               color=color, legend_label=legend_label)
+
+    graph.legend.location = "top_left"
+
+    show(graph)
+
+
+generate_visualizations("TMUS")
