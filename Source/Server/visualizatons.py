82fc6dd0f2960c1d94b374db91e4aa41e58d8998
Evan Harrigan <harriganevan@gmail.com>
2023-04-15 11:58:22 -0400

    added metrics

--- Source/Server/visualizatons.py
+++ Source/Server/visualizatons.py
@@ -3,7 +3,7 @@ from cassandra.cluster import Cluster
 from bokeh.plotting import figure, output_file, show
 
 # replace localhost with your Cassandra host IP
-cluster = Cluster(['localhost'])
+cluster = Cluster(['104.196.157.153'])
 session = cluster.connect()
 
 
@@ -18,17 +18,34 @@ def queryCluster(ticker):
 
 def generate_visualizations(ticker):
     data = queryCluster(ticker)
+
+    # Visualize metrics
+    moving_avg = data.moving_avg
+    graphHelper(ticker, moving_avg, "MOVING AVERAGE", data)
+
+    daily_percentage = data.daily_percentage
+    graphHelper(ticker, daily_percentage, "DAILY PERCENTAGE", data)
+
+    atr = data.atr
+    graphHelper(ticker, atr, "ATR", data)
+
+    vpt = data.vpt
+    graphHelper(ticker, vpt, "VPT", data)
+
+    rsi = data.rsi
+    graphHelper(ticker, rsi, "RSI", data)
+
+
+def graphHelper(ticker, yaxis, title, data):
     dates = [date.date() for date in data.dates]
 
-    # Visualize ATR
-    atr = data.atr
-    output_file("atr.html")
-    graph = figure(x_axis_type="datetime", title="{} ATR".format(ticker))
+    output_file("output.html")
+    graph = figure(x_axis_type="datetime", title="{} {}".format(ticker, title))
     graph.xaxis.axis_label = 'Date'
-    graph.yaxis.axis_label = 'ATR (14 day)'
+    graph.yaxis.axis_label = '{} (14 day)'.format(title)
 
     x_axis_coordinates = np.array(dates, dtype=np.datetime64)
-    y_axis_coordinates = np.array(atr)
+    y_axis_coordinates = np.array(yaxis)
     color = "lightblue"
     legend_label = ticker
     graph.line(x_axis_coordinates, y_axis_coordinates,
@@ -37,6 +54,5 @@ def generate_visualizations(ticker):
     graph.legend.location = "top_left"
 
     show(graph)
-
 
 generate_visualizations("TMUS")
